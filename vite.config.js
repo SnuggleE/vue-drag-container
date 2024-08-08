3be8bf9d5e35591d602c4076918445e9a4590516
liujiankun <353239468@qq.com>
2024-08-08 09:14:10 +0800

    fix

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,23 @@
 import { defineConfig } from 'vite'
+import { resolve } from 'path'
 import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  build: {
+    lib: {
+      entry: resolve(__dirname, 'src/build.js'),
+      name: 'vue-drag-container',
+    },
+    rollupOptions: {
+      external: ['vue'],
+      output: {
+        globals: {
+          vue: 'Vue',
+        },
+        
+      }
+    },
+  },
   plugins: [vue()],
 })
